772fa19810bdddefad0a49628713986b36cd9c5b
Starling Germosen <sgermosen@outlook.com>
2019-06-22 11:58:38 -0400

    update

---
+++ EventsCoach/EcCoach.Web/Migrations/20190622154859_DataForType.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcCoach.Web.Migrations
+{
+    public partial class DataForType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+             table: "Types",
+             columns: new[] { "Id", "Name" },
+             values: new object[] { 1, "Personal Superation" });
+
+            migrationBuilder.InsertData(
+         table: "Types",
+         columns: new[] { "Id", "Name" },
+         values: new object[] { 2, "Talk" });
+
+            migrationBuilder.InsertData(
+          table: "Types",
+          columns: new[] { "Id", "Name" },
+          values: new object[] { 3, "Conference" });
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+                     migrationBuilder.DeleteData(
+                    table: "Types",
+                    keyColumn: "Id",
+                    keyValue: 1);
+
+                    migrationBuilder.DeleteData(
+                    table: "Types",
+                    keyColumn: "Id",
+                    keyValue: 2);
+
+                     migrationBuilder.DeleteData(
+                    table: "Types",
+                    keyColumn: "Id",
+                    keyValue: 3);
+
+
+        }
+    }
+}
